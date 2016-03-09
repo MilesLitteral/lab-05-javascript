bc5c130828932dd25b3e63c349d518dd3857a16e
Student <student@ent-v314-005.local>
2016-03-09 11:20:01 -0500

    Best HTML

--- myScript.js
+++ myScript.js
@@ -1,3 +1,12 @@
+var userName;
+
+function periodicHello(){
+	console.log(userName);
+}
+
 $(document).ready(function(){ 
 	console.log("Hello World");
+	var userMessage = prompt("Whats your name");
+	userName = userMessage;
+	d3.select("#big").text(d3.select("#big").text() + userName);
 })
